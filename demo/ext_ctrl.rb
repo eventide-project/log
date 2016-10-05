c82e6b9dbfb0a4920b267f2fadbead9317cfc208
Scott Bellware <sbellware@gmail.com>
2016-10-05 03:54:36 -0500

    External control

--- demo/ext_ctrl.rb
+++ demo/ext_ctrl.rb
@@ -2,16 +2,18 @@ require_relative '../init'
 require 'test_bench'; TestBench.activate
 require 'pp'
 
-# # Default levels and color for default level
-# logger = Log.build('Levels and Colors for Min Level')
+# Default levels and color for default level
+logger = Log.build('Levels and Colors for Min Level')
 
-# logger.level_names.each do |level_name|
-#   logger.("some #{level_name} message", level_name)
-# end
+logger.level_names.each do |level_name|
+  logger.("some #{level_name} message", level_name)
+end
 
 
 # Default tags
-logger = Log.no_defaults('Default Tags Demo')
+logger = Log.build('Default Tags Demo')
+
+logger.no_level!
 
 logger.('some tagged message', tags: [:some_tag, :some_other_tag])
 logger.('some other tagged message', tags: [:some_other_tag, :yet_another_tag])
