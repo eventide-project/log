079c3ca4a7131b52ea980e41e23785bb9ceda747
Scott Bellware <sbellware@gmail.com>
2016-10-04 19:53:32 -0500

    Filter concern

--- lib/log/log.rb
+++ lib/log/log.rb
@@ -5,6 +5,7 @@ class Log
   include Levels
   extend Telemetry::Register
   include Level
+  include Filter
   include SubjectName
 
   initializer :subject
@@ -70,44 +71,6 @@ class Log
     return false if level.nil? && !self.level.nil?
     precedent?(level) && write_tag?(tags)
   end
-
-  ## filter concern
-  def write_tag?(message_tags)
-    message_tags ||= []
-
-    if message_tags.empty? && !logger_tags?
-      return true
-    end
-
-    if !message_tags.empty? && log_all_tags?
-      return true
-    end
-
-    if message_tags.empty? && log_untagged?
-      return true
-    end
-
-    if !message_tags.empty? && logger_tags?
-      if logger_tags_intersect?(message_tags)
-        return true
-      end
-    end
-
-    false
-  end
-
-  def log_all_tags?
-    logger_tag?(:_all)
-  end
-
-  def log_untagged?
-    logger_tag?(:_untagged)
-  end
-
-  def tags_intersect?(message_tags)
-    !(logger_tags & message_tags).empty?
-  end
-  alias :logger_tags_intersect? :tags_intersect?
 
   ## tag concern
   def tag?(tag)
