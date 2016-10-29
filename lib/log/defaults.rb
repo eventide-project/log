046e7bde53f1154d463159e3fb560734b0863052
Scott Bellware <sbellware@gmail.com>
2016-10-29 12:06:41 -0500

    Debug and trace colors are swapped

--- lib/log/defaults.rb
+++ lib/log/defaults.rb
@@ -60,8 +60,8 @@ module Log::Defaults
       error: proc { |message| Rainbow(message).bright.white.bg(:red) },
       fatal: proc { |message| Rainbow(message).red.bg(:black) },
       warn: proc { |message| Rainbow(message).yellow.bg(:black) },
-      debug: proc { |message| Rainbow(message).cyan },
-      trace: proc { |message| Rainbow(message).green }
+      debug: proc { |message| Rainbow(message).green },
+      trace: proc { |message| Rainbow(message).cyan }
     }
   end
 end
