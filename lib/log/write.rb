da4675302c27bfee0e6ada5777b4f2a8d89c730e
Nathan Ladd <nathanladd@gmail.com>
2020-08-19 09:53:18 -0500

    Newline character is derived from IO#puts, not  (which isn't correct)

--- lib/log/write.rb
+++ lib/log/write.rb
@@ -9,13 +9,17 @@ class Log
 
       line = Log::Format.line(message, clock.iso8601(precision: 5), subject, level, device, &levels[level] &.message_formatter)
 
-      puts "#{line}#{$INPUT_RECORD_SEPARATOR}"
+      puts "#{line}#{Write.newline_character}"
 
       telemetry.record :logged, Log::Telemetry::Data.new(subject, message, level, tags, line)
     end
 
     def puts(message)
       device.write(message)
+    end
+
+    def self.newline_character
+      @newline_character ||= StringIO.new.tap { |io| io.puts('') }.string
     end
   end
 end
